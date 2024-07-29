e4080a0222c91c316a67ff14b16ee3419d630f8f
aditya <adisingh925@gmail.com>
2024-07-29 23:00:00 +0530

    na

--- logging/postLogsToGoogleChat.js
+++ logging/postLogsToGoogleChat.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 require("dotenv").config();
 
 async function postLogToGoogleChat(errorMessage) {
+    console.log(process.env.NODE_ENV);
     if (process.env.NODE_ENV === 'production') {
         try {
             await axios.post(
