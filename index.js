bcc8ad20be2a53c42cfbae5f1628248c0ac9cf3a
aditya <adisingh925@gmail.com>
2024-08-02 23:46:20 +0530

    na

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ const keyFilePath = path.join(__dirname, 'ssl', 'private.key');
 const certFilePath = path.join(__dirname, 'ssl', 'certificate.crt');
 const caFilePath = path.join(__dirname, 'ssl', 'ca_bundle.crt');
 
-const app = uWS.SSLApp({
-  key_file_name: keyFilePath,
-  cert_file_name: certFilePath,
-  ca_file_name: caFilePath,
-}).ws('/', {
+const app = uWS.App({
+  key_file_name: 'misc/key.pem',
+  cert_file_name: 'misc/cert.pem',
+  passphrase: '1234'
+}).ws('/*', {
   compression: uWS.SHARED_COMPRESSOR,
   maxPayloadLength: 16 * 1024 * 1024,
 
