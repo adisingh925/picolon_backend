3c9673a0e865e1146cf6b6d34be61edfcaaeb2a4
aditya <adisingh925@gmail.com>
2024-08-03 16:26:49 +0530

    na

--- index.js
+++ index.js
@@ -17,18 +17,18 @@ const socketToRoom = new Map();
 var connections = 0;
 
 // Port to listen
-const port = 9001;
+const port = 443;
 
 // Certificate Path SSL/TLS certificate files
 const keyFilePath = path.join(__dirname, 'ssl', 'private.key');
 const certFilePath = path.join(__dirname, 'ssl', 'certificate.crt');
 const caFilePath = path.join(__dirname, 'ssl', 'ca_bundle.crt');
 
-const app = uWS.App({
+const app = uWS.SSLApp({
   key_file_name: keyFilePath,
   cert_file_name: certFilePath,
   ca_file_name: caFilePath,
-}).ws('/*', {
+}).ws('/', {
   compression: uWS.SHARED_COMPRESSOR,
   maxPayloadLength: 1048576,
   maxLifetime: 0,
