e1365bb722057bcb6eea38c3a5d23e02340deebc
aditya <adisingh925@gmail.com>
2024-10-01 01:07:46 +0530

    na

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ const targetUrl = 'https://googleads.g.doubleclick.net';
 uWS.SSLApp({
   key_file_name: keyFilePath,
   cert_file_name: certFilePath,
-  ssl_ciphers: 'TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:ECDHE-RSA-AES256-SHA',
+  ssl_ciphers: 'TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256:ECDHE-RSA-AES256-SHA',
 }).ws('/', {
   compression: uWS.SHARED_COMPRESSOR,
   maxPayloadLength: 1048576,
